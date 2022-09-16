59471b3f4684b2d77022bd3826a4d4b1ee237509
Romero Gonçalves <romerotg@gmail.com>
2022-09-16 17:24:48 -0300

    adding empty list content to ads page when there is no ad

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { View, TouchableOpacity, Image, FlatList } from 'react-native';
+import { View, TouchableOpacity, Image, FlatList, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRoute, useNavigation } from '@react-navigation/native';
 import { Entypo } from '@expo/vector-icons';
@@ -80,8 +80,13 @@ export function Game() {
                     }}
                     horizontal
                     style={styles.containerList}
-                    contentContainerStyle={styles.contentList}
+                    contentContainerStyle={ads.length > 0 ? styles.contentList : styles.emptyListContent}
                     showsHorizontalScrollIndicator={false}
+                    ListEmptyComponent={() => (
+                        <Text style={styles.emptyListText}>
+                            Não há anúncios publicados ainda.
+                        </Text>
+                    )}
                 />
             </SafeAreaView>
         </Background>
