805b3fc98602a2c06ad1b099c37418072ae8fa5d
Romero Gonçalves <romerotg@gmail.com>
2022-09-17 17:19:22 -0300

    Enhancement to discord screen so the user can copy the discord id

--- src/components/DuoMatch/index.tsx
+++ src/components/DuoMatch/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { View, Modal, ModalProps, Text, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import { View, Modal, ModalProps, Text, TouchableOpacity, Alert, ActivityIndicator } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { CheckCircle } from 'phosphor-react-native';
+import * as Clipboard from 'expo-clipboard';
 
 import { styles } from './styles';
 import { THEME } from '../../theme';
@@ -14,6 +15,15 @@ interface Props extends ModalProps {
 }
 
 export function DuoMatch({ discord, onClose, ...rest}: Props) {
+  const [isCopying, setIsCopying] = useState<boolean>(false);
+
+  async function handleCopyDiscordToClipboard() {
+    setIsCopying(true);
+    await Clipboard.setStringAsync(discord);
+    Alert.alert('Discord copiado!', 'Usuário copiado para você colar no Discord.');
+    setIsCopying(false);
+  }
+
   return (
     <Modal
         animationType="fade"
@@ -48,9 +58,13 @@ export function DuoMatch({ discord, onClose, ...rest}: Props) {
                     Adicione no Discord
                 </Text>
 
-                <TouchableOpacity style={styles.discordButton}>
+                <TouchableOpacity
+                    style={styles.discordButton}
+                    onPress={handleCopyDiscordToClipboard}
+                    disabled={isCopying}
+                >
                     <Text style={styles.discord}>
-                        {discord}
+                        {isCopying ? <ActivityIndicator color={THEME.COLORS.PRIMARY} /> : discord}
                     </Text>
                 </TouchableOpacity>
             </View>
